115765e5c77531457d37cd135d05ef3bb33e3956
Lebogang PETSOANE <lpetsoan@c4r16s8.wethinkcode.co.za>
2019-05-18 10:53:06 +0200

    Pushing to main"

---
+++ ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpetsoan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/18 08:47:26 by lpetsoan          #+#    #+#             */
+/*   Updated: 2019/05/18 09:15:02 by lpetsoan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int pos, char c))
+{
+	char 	*str;
+	int 	size;
+	int		i;
+
+	size = 0;
+	i = 0;
+	while (s[size])
+		size++;
+	str = (char *)malloc(size);
+	if (!str)
+		return NULL;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	return (str);
+}
