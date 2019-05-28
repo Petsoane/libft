31c1646f1b58e307ca21ab743e94dcc39aec61bb
Lebogang PETSOANE <lpetsoan@c4r16s8.wethinkcode.co.za>
2019-05-28 16:31:39 +0200

    do not decrement before in check

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,30 +6,36 @@
 /*   By: lpetsoan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:19:21 by lpetsoan          #+#    #+#             */
-/*   Updated: 2019/05/28 13:25:26 by lpetsoan         ###   ########.fr       */
+/*   Updated: 2019/05/28 16:30:43 by lpetsoan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char	*ft_strnstr(const char *n, const char *h, size_t size)
+char	*ft_strnstr(const char *h, const char *n, size_t size)
 {
 	int		i;
+	int		size_n;
 
 	i = 0;
-	while (*h && size--)
+	size_n = ft_strlen(n);
+	if (size_n == 0)
+		return ((char *)h);
+	while (*h && size)
 	{
-			i = 0;
-		while (*(h + i) == n[i] && size--)
+		i = 0;
+		while (*(h + i) == n[i] && size)
 		{
 			if (n[i + 1] == '\0')
 			{
 				return ((char *)h);
 			}
 			i++;
+			size--;
 		}
 		h++;
+		size--;
 	}
 	return (NULL);
 
